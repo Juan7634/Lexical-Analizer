bc9ceeab635504972e59a427584ee9904b1e21b4
Juan Pedro <juan.dominguez2763@gmail.alumnos.udg.mx>
2022-01-29 16:00:49 -0600

    Se ha creado el contador de lineas y palabras

---
+++ src/analizer/lex/contador_lineas.java
@@ -0,0 +1,30 @@
+package analizer.lex;
+
+public class contador_lineas {
+    private int line;
+    private int word;
+    
+        public contador_lineas(){
+            line = 0;
+            word = 0;
+        }
+
+        public void setLine(){
+            this.line += 1;
+        }
+        
+        public void setword(){
+            this.word += 1;
+        }
+
+        public void clear_word(){
+            this.word = 0;
+        }
+
+        public int getLine(){
+            return line;
+        }
+        public int getWord(){
+            return word;
+        }
+}
